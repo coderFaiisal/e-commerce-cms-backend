fe78d14873b9e4eb6f34f69adcb9e7c2be289111
Faisal <officialfaisal1112@gmail.com>
2023-11-01 22:13:23 +0600

    create all modules folder

--- src/app/modules/cart/cart.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Types } from 'mongoose';
-
-export type ICart = {
-  userEmail: string;
-  products: {
-    productId: Types.ObjectId;
-    quantity: string;
-  }[];
-};
--- src/app/modules/wishList/wishList.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Types } from 'mongoose';
-
-export type IWishList = {
-  userEmail: string;
-  productId: Types.ObjectId;
-};

eef00e11414a443e2a2faf9e8c7f2324887d4369
Faisal <officialfaisal1112@gmail.com>
2023-11-04 00:01:25 +0600

    add route files

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,9 @@ import { AuthRoutes } from '../modules/auth/auth.route';
 import { OrderRoutes } from '../modules/order/order.route';
 import { ProductRoutes } from '../modules/product/product.route';
 import { UserRoutes } from '../modules/user/user.route';
+import { StoreRoutes } from '../modules/store/store.route';
+import { BillboardRoutes } from '../modules/billboard/billboard.route';
+import { CategoryRoutes } from '../modules/category/category.route';
 
 const router = express.Router();
 
@@ -21,13 +24,34 @@ const moduleRoutes = [
     route: AdminRoutes,
   },
   {
+    path: '/stores',
+    route: StoreRoutes,
+  },
+  {
+    path: '/billboards',
+    route: BillboardRoutes,
+  },
+  {
+    path: '/categories',
+    route: CategoryRoutes,
+  },
+  {
     path: '/products',
     route: ProductRoutes,
   },
+  //
+  // {
+  //   path: '/carats',
+  //   route: CaratRoutes,
+  // },
   {
     path: '/orders',
     route: OrderRoutes,
   },
+  // {
+  //   path: '/addReviews',
+  //   route: AddReviewRoutes,
+  // },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));

fb12106cd10d247ecf450b5cdaee2b4eaa8633bd
Faisal <officialfaisal1112@gmail.com>
2023-11-04 19:02:52 +0600

    setup store route

--- src/app/modules/store/store.route.ts
+++ src/app/modules/store/store.route.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
-router.post('/');
+router.post('/create-store', auth(ENUM_USER_ROLE.ADMIN));
+
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN));
+
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN));
+
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN));
 
 export const StoreRoutes = router;

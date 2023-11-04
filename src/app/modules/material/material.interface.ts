fbd35386123008131ff54940c670324b47fef0d6
Faisal <officialfaisal1112@gmail.com>
2023-11-04 23:13:24 +0600

    complete material interface

--- src/app/modules/material/material.interface.ts
+++ src/app/modules/material/material.interface.ts
@@ -1,3 +1,13 @@
+import { Model, Types } from 'mongoose';
+import { IProduct } from '../product/product.interface';
+import { IStore } from '../store/store.interface';
+
 export type IMaterial = {
+  _id?: string;
   name: string;
+  value: string;
+  storeId: Types.ObjectId | IStore;
+  products: Types.ObjectId[] | IProduct[];
 };
+
+export type MaterialModel = Model<IMaterial, Record<string, undefined>>;

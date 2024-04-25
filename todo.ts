091807c967bea1fd13ad8335762739511d52e313
Faisal <officialfaisal1112@gmail.com>
2024-04-25 21:16:32 +0600

    take some notes and decide next step.

--- todo.ts
+++ todo.ts
@@ -1,5 +1,38 @@
 // * Explore and implement Invoice
-// * Implement logic in where store-owner will notify about the subscription time expiration.
-// * Complete payment service. look on order and subscription module service logic.
+// * Implement logic in where store-owner will notify about the subscription time expiration. After the subscription time end, database will update and subscription isActive=false and other related things will happened.
+// * Notify user and store owner or admin after payment ipn completed.
 // * Create more analytics query based on requirements.
 // * Research further more and try to complete backend.
+
+// * Send email after order and subscription completed.
+// * notification = {title: string, message: string, status: "read" | "unread", userId: ObjectId}
+
+// * cron-job for automatically done something.
+// * node-cron - package
+
+// cron.schedule('0 0 0 * * *', async () => {
+//   const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+//   await Notification.deleteMany({
+//     status: 'read',
+//     createdAt: { $lt: thirtyDaysAgo },
+//   });
+// });
+
+// Redis
+
+// import {Redis} from 'ioredis';
+// import config from './config';
+
+// const redisClient = () => {
+//   if (config.redis_url) {
+//     console.log('Redis connected...');
+
+//     return config.redis_url;
+//   }
+
+//   throw new Error('Failed to connect Redis.');
+// };
+
+// export const redis = new Redis(redisClient());
+
+// await redis.set(productId, JSON.stringify(product), 'EX', 604800)
